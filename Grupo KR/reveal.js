22267c93003ed991a595da8ba9d7a1337619796c
Miza_Dev <mizaelwgr@gmail.com>
2024-04-02 23:05:11 -0300

    Add files via upload

---
+++ Grupo KR/reveal.js
@@ -0,0 +1,52 @@
+
+window.revelar = ScrollReveal({reset:true})
+
+// topo do site
+revelar.reveal('.text-topo',
+{
+    duration: 2000,
+    distance:'90px'
+})
+
+revelar.reveal('.img-topo',
+{
+    duration: 2000,
+    distance:'90px',
+    delay: 500
+
+})
+
+revelar.reveal('.rh',
+{
+    duration: 2000,
+    distance:'90px'
+
+})
+
+revelar.reveal('.sobre-rh',
+{
+    duration: 2000,
+    distance:'90px'
+
+})
+
+revelar.reveal('.seja-rh',
+{
+    duration: 2000,
+    distance:'90px'
+
+})
+
+revelar.reveal('.parceiros-rh',
+{
+    duration: 2000,
+    distance:'90px'
+
+})
+
+revelar.reveal('.contato-rh',
+{
+    duration: 2000,
+    distance:'90px'
+
+})
